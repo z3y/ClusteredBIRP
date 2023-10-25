84790655e7b4fc500ba45c5d7967c31791a3e680
z3y <33181641+z3y@users.noreply.github.com>
2023-10-25 22:46:10 +0200

    dynamic updates toggle

--- Runtime/CBIRPManager.cs
+++ Runtime/CBIRPManager.cs
@@ -17,11 +17,14 @@ namespace CBIRP
         public Texture2D shadowmask;
         public CubemapArray reflectionProbeArray;
         //public Cubemap skyProbe;
+        [SerializeField] private Camera _trackingCamera;
+        [Tooltip("Enable updates to any of the light or probe variables at runtime (Position, Rotation, Color, Range etc). Disable to skip the additional camera used to track them")]
+        [SerializeField] private bool _dynamicUpdates = true;
 
         private void Start()
         {
             SetGlobalUniforms();
-            Destroy(this);
+            SetDynamicUpdatesState(_dynamicUpdates);
         }
 
         public void SetGlobalUniforms()
@@ -33,6 +36,15 @@ namespace CBIRP
             VRCShader.SetGlobalTexture(VRCShader.PropertyToID("_Udon_CBIRP_ReflectionProbes"), reflectionProbeArray);
             //VRCShader.SetGlobalTexture(VRCShader.PropertyToID("_Udon_CBIRP_SkyProbe"), skyProbe);
         }
+
+        public void SetDynamicUpdatesState(bool isEnabled)
+        {
+            SendCustomEventDelayedFrames(
+                isEnabled ? nameof(EnableDynamicUpdates) : nameof(DisableDynamicUpdates),
+                5, VRC.Udon.Common.Enums.EventTiming.Update);
+        }
+        public void DisableDynamicUpdates() => _trackingCamera.gameObject.SetActive(false);
+        public void EnableDynamicUpdates() => _trackingCamera.gameObject.SetActive(true);
 
 #if UNITY_EDITOR && !COMPILER_UDONSHARP
         public void OnValidate()
