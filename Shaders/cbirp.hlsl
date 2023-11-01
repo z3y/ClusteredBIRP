9faf75b2a735cb9d6e9db802d0a2208f34c98633
z3y <33181641+z3y@users.noreply.github.com>
2023-11-01 11:34:13 +0100

    improve cluster loop performance a bit

--- Shaders/cbirp.hlsl
+++ Shaders/cbirp.hlsl
@@ -14,22 +14,22 @@ Texture2D _Udon_CBIRP_ShadowMask;
 
 #define CBIRP_TYPE_LIGHT 0
 #define CBIRP_TYPE_PROBE 1
-static uint cbirpTempIndexable[4];
+static uint cbirpTempIndexable[3];
 
 #define CBIRP_CLUSTER_START(cluster, type) \
     uint4 flags4x = _Udon_CBIRP_Clusters[uint2(type ? 3 : 0, cluster.x)]; \
     uint4 flags4y = _Udon_CBIRP_Clusters[uint2(type ? 4 : 1, cluster.y)]; \
     uint4 flags4z = _Udon_CBIRP_Clusters[uint2(type ? 5 : 2, cluster.z)]; \
     uint4 flags4 = flags4x & flags4y & flags4z; \
-    cbirpTempIndexable[0] = flags4.x; \
-    cbirpTempIndexable[1] = flags4.y; \
-    cbirpTempIndexable[2] = flags4.z; \
-    cbirpTempIndexable[3] = flags4.w; \
+    cbirpTempIndexable[0] = flags4.y; \
+    cbirpTempIndexable[1] = flags4.z; \
+    cbirpTempIndexable[2] = flags4.w; \
+    uint flags = flags4.x; \
     uint component = 0; \
-    while (component < 4) { \
-        [branch] if (cbirpTempIndexable[component] == 0) { component++; continue; } \
-        uint index = firstbitlow(cbirpTempIndexable[component]); \
-        cbirpTempIndexable[component] ^= 0x1 << index; \
+    while (component < 3) { \
+        [branch] if (flags == 0) { flags = cbirpTempIndexable[component]; component++; continue; } \
+        uint index = firstbitlow(flags); \
+        flags ^= 0x1 << index; \
         index += 32 * component; \
 
 #define CBIRP_CLUSTER_END \
